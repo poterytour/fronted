536adbc309233bbf20f9ef4b4b0251472c315f52
palpitate-wx <2281342596@qq.com>
2023-02-13 11:32:13 +0800

    声明文件更新

---
+++ app/src/main/java/com/example/poetrytour/ui/post/PostActivity.kt
@@ -0,0 +1,12 @@
+package com.example.poetrytour.ui.post
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.poetrytour.R
+
+class PostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post)
+    }
+}
