e20c5658805ffc56cc29ac295465288816aba3c5
lenovo <324922785@qq.com>
2023-02-26 15:26:09 +0800

    合并

--- app/src/main/java/com/example/poetrytour/ui/mine/MineAboutActivity.kt
+++ app/src/main/java/com/example/poetrytour/ui/mine/MineAboutActivity.kt
@@ -1,10 +1,11 @@
 package com.example.poetrytour.ui.mine
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
 import android.widget.TextView
 import com.example.poetrytour.R
+import com.makeramen.roundedimageview.RoundedImageView
 import java.io.InputStream
 
 class MineAboutActivity : AppCompatActivity() {
@@ -12,10 +13,11 @@ class MineAboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mine_about)
         val text:TextView = findViewById(R.id.app_jieshao)
-        val img:ImageView = findViewById(R.id.app_icon)
+        val img:RoundedImageView = findViewById(R.id.app_icon)
         val get = GetPrivacyFile()
-        val inputStream:InputStream=resources.openRawResource(R.raw.abstract_privacy)
+        val inputStream:InputStream=resources.openRawResource(R.raw.app_introduce)
+        text.setTypeface(Typeface.SERIF,Typeface.BOLD)
         text.text = get.getString(inputStream)
-        img.setImageResource(R.drawable.img_tx)
+        img.setImageResource(R.drawable.app_icon)
     }
 }
